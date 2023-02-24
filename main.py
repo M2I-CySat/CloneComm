bab483f87793922b4cc70c8924db6331b6585f66
SarahDeMatteis <59626582+SarahDeMatteis@users.noreply.github.com>
2023-02-23 22:22:04 -0600

    Added subsystem tabs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import ttk
 
+#Use struct to send data (packets)?
+
 #Commands for basic packet-sending testing
 def send_packet():
     print("Packet Send Button Test")
@@ -8,17 +10,44 @@ def send_packet():
 def req_packet():
     print("Packet Request Button Test")
 
+#Creates the main application window
 root = Tk()
 root.title("CloneComms")
-
 mainframe = ttk.Frame(root, padding=5)
-titlelbl = ttk.Label(mainframe, text="Commands")
-send_packet_btn = ttk.Button(mainframe, text="Send Packet", command=send_packet)
-req_packet_btn = ttk.Button(mainframe, text="Request Packet", command=req_packet)
+titlelbl = ttk.Label(mainframe, text="CySat Commands", font="TkHeadingFont")
 
-mainframe.grid(column=0,row=0)
-titlelbl.grid(row=0, column=0)
-send_packet_btn.grid(row=1, column=0)
-req_packet_btn.grid(row=2, column=0)
+#Organizes widgets inside the main window
+mainframe.grid(row=0, column=0)
+titlelbl.grid(row=0, column=0, columnspan=3)
+
+#Creates tabs for each subsystem
+
+#NOT YET IMPLEMENTED:
+#Each subsystem will have a dropdown menu of command options
+#A text input box will be available if manual input is required for a command
+
+tab_interface = ttk.Notebook(mainframe, padding=5)
+tab_interface.grid(row=1,column=1,rowspan=3,sticky=NSEW)
+
+test_tab = ttk.Frame(tab_interface)
+obc_tab = ttk.Frame(tab_interface)
+adcs_tab = ttk.Frame(tab_interface)
+sdr_tab = ttk.Frame(tab_interface)
+eps_tab = ttk.Frame(tab_interface)
+uhf_tab = ttk.Frame(tab_interface)
+
+tab_interface.add(test_tab, text='Test Commands')
+#Button to send packet (for testing)
+send_packet_btn = ttk.Button(test_tab, text="Send Packet", command=send_packet, padding=5)
+send_packet_btn.grid(column=1,row=0)
+#Button to request a packet (for testing)
+req_packet_btn = ttk.Button(test_tab, text="Request Packet", command=req_packet, padding=5)
+req_packet_btn.grid(column=0,row=0)
+
+tab_interface.add(obc_tab, text='OBC Commands')
+tab_interface.add(adcs_tab, text='ADCS Commands')
+tab_interface.add(sdr_tab, text='SDR Commands')
+tab_interface.add(eps_tab, text='EPS Commands')
+tab_interface.add(uhf_tab, text='UHF Commands')
 
 root.mainloop()
