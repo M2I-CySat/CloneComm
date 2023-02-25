5fdd46d384dffe71a49c6795015586b5bb733195
SarahDeMatteis <59626582+SarahDeMatteis@users.noreply.github.com>
2023-02-24 21:53:07 -0600

    Updated dropdown box to use command manifest

--- main.py
+++ main.py
@@ -1,7 +1,86 @@
 from tkinter import *
 from tkinter import ttk
-
+import importlib
 #Use struct to send data (packets)?
+
+#Imports CySatGlobal for command information
+global_module = importlib.import_module('CySatGlobal')
+cmd_dictionary = global_module.cmd_dictionary
+sys_dictionary = global_module.sys_dictionary
+sys_list = global_module.sys_list
+
+global_module.populate_global_variables()
+
+#Creates the main application window
+root = Tk()
+root.title("CloneComms")
+mainframe = ttk.Frame(root, padding=5)
+ttk.Label(mainframe, text="CySat Commands", font="TkHeadingFont").grid(row=0, column=0, columnspan=3)
+
+#Initializes textvariables for dropdown menus
+obc_command = StringVar()
+adcs_command = StringVar()
+sdr_command = StringVar()
+eps_command = StringVar()
+uhf_command = StringVar()
+packet_input = StringVar()
+
+#Organizes widgets inside the main window
+mainframe.grid(row=0, column=0)
+
+#Creates tabs for each subsystem
+tab_interface = ttk.Notebook(mainframe, padding=5)
+tab_interface.grid(row=1,column=1,rowspan=3,sticky=NSEW)
+
+test_tab = ttk.Frame(tab_interface,padding=5)
+obc_tab = ttk.Frame(tab_interface,padding=5)
+adcs_tab = ttk.Frame(tab_interface,padding=5)
+sdr_tab = ttk.Frame(tab_interface,padding=5)
+eps_tab = ttk.Frame(tab_interface,padding=5)
+uhf_tab = ttk.Frame(tab_interface,padding=5)
+
+######################################################################################################################
+#Test Tab (currently in use for testing interface functions, will be removed later)
+
+tab_interface.add(test_tab, text='Test')
+
+#Populates command list for the selected subsystem (in this case, test (id = 1))
+#Will be used for other subsystems
+cmd_list = cmd_dictionary[1]
+dropdown_cmd_list = []
+for key, value in cmd_list.items():
+    if(value.cmd_sendable == 1):
+        dropdown_cmd_list.append(value.cmd_description)
+
+#Creates a dropdown menu for command options
+ttk.Label(test_tab, text="Choose Command:").grid(column=0,row=0,sticky=W)
+test_command = StringVar(test_tab)
+test_combobox = ttk.Combobox(test_tab,textvariable=test_command,values=dropdown_cmd_list,state=(["readonly"]))
+test_combobox.grid(column=2,row=0)
+
+#Creates an input box
+ttk.Label(test_tab, text="Enter Custom Packet:").grid(column=0,row=2)
+test_entry = ttk.Entry(test_tab, textvariable=packet_input).grid(column=1,row=2,columnspan=3, sticky=EW)
+
+#Function to retrieve input from the entry box
+def get_input():
+    print(packet_input.get())
+
+#Button to send packet (for testing)
+send_custom_btn = ttk.Button(test_tab, text="Send Entry", command=get_input)
+send_custom_btn.grid(column=2,row=3,sticky=E)
+
+#If nothing has been entered, disables the "Send Entry" button
+#Updates any time the user changes the text in the entry box
+def set_button_state(var,index,mode):
+    if len(packet_input.get()) == 0:
+        send_custom_btn.state(['disabled'])
+    else:
+        send_custom_btn.state(['!disabled'])
+packet_input.trace_add("write", set_button_state)
+
+######################################################################################################################
+#OBC Tab (currently used for testing buttons)
 
 #Commands for basic packet-sending testing
 def send_packet():
@@ -10,59 +89,17 @@ def send_packet():
 def req_packet():
     print("Packet Request Button Test")
 
-#Initializes textvariables for dropdown menus
-test_command = "Choose command..."
-obc_command = "Choose command..."
-adcs_command = "Choose command..."
-sdr_command = "Choose command..."
-eps_command = "Choose command..."
-uhf_command = "Choose command..."
+#Adds OBC tab to interface
+tab_interface.add(obc_tab, text='  OBC  ')
+send_packet_btn = ttk.Button(obc_tab, text="Send Packet", command=send_packet, padding=5).grid(column=0,row=0)
+req_packet_btn = ttk.Button(obc_tab, text="Request Packet", command=req_packet, padding=5).grid(column=1,row=0)
 
-#Creates the main application window
-root = Tk()
-root.title("CloneComms")
-mainframe = ttk.Frame(root, padding=5)
-ttk.Label(mainframe, text="CySat Commands", font="TkHeadingFont").grid(row=0, column=0, columnspan=3)
+######################################################################################################################
+#More tabs (commands will be added later)
 
-#Organizes widgets inside the main window
-mainframe.grid(row=0, column=0)
-
-#Creates tabs for each subsystem
-
-#NOT YET IMPLEMENTED:
-#Each subsystem will have a dropdown menu of command options
-#A text input box will be available if manual input is required for a command
-
-tab_interface = ttk.Notebook(mainframe, padding=5)
-tab_interface.grid(row=1,column=1,rowspan=3,sticky=NSEW)
-
-test_tab = ttk.Frame(tab_interface)
-obc_tab = ttk.Frame(tab_interface)
-adcs_tab = ttk.Frame(tab_interface)
-sdr_tab = ttk.Frame(tab_interface)
-eps_tab = ttk.Frame(tab_interface)
-uhf_tab = ttk.Frame(tab_interface)
-
-tab_interface.add(test_tab, text='Test Commands')
-
-#Creates a dropdown menu for command options
-
-test_combobox = ttk.Combobox(test_tab,textvariable=test_command,values=['Test 1','Test 2'])
-test_combobox.grid(column=1,row=0)
-ttk.Label(test_tab, text="Choose Command:", padding=5).grid(column=0,row=0)
-
-#Button to send packet (for testing)
-send_packet_btn = ttk.Button(test_tab, text="Send Packet", command=send_packet, padding=5)
-send_packet_btn.grid(column=1,row=2)
-
-#Button to request a packet (for testing)
-req_packet_btn = ttk.Button(test_tab, text="Request Packet", command=req_packet, padding=5)
-req_packet_btn.grid(column=0,row=2)
-
-tab_interface.add(obc_tab, text='OBC Commands')
-tab_interface.add(adcs_tab, text='ADCS Commands')
-tab_interface.add(sdr_tab, text='SDR Commands')
-tab_interface.add(eps_tab, text='EPS Commands')
-tab_interface.add(uhf_tab, text='UHF Commands')
+tab_interface.add(adcs_tab, text='  ADCS  ')
+tab_interface.add(sdr_tab, text='  SDR  ')
+tab_interface.add(eps_tab, text='  EPS  ')
+tab_interface.add(uhf_tab, text='  UHF  ')
 
 root.mainloop()
