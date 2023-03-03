12df8ea76b6dbc2d1b2a3ad3f23cb1f224434bc8
SarahDeMatteis <59626582+SarahDeMatteis@users.noreply.github.com>
2023-03-02 18:03:13 -0600

    Added functions to get commands for different tabs

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ ttk.Label(mainframe, text="CySat Commands", font="TkHeadingFont").grid(row=0, co
 
 #Initializes textvariables for dropdown menus
 obc_command = StringVar()
-adcs_command = StringVar()
 sdr_command = StringVar()
 eps_command = StringVar()
 uhf_command = StringVar()
@@ -39,30 +38,48 @@ sdr_tab = ttk.Frame(tab_interface,padding=5)
 eps_tab = ttk.Frame(tab_interface,padding=5)
 uhf_tab = ttk.Frame(tab_interface,padding=5)
 
+tab_interface.add(test_tab, text='Test')
+tab_interface.add(obc_tab, text='  OBC  ')
+tab_interface.add(adcs_tab, text='  ADCS  ')
+tab_interface.add(sdr_tab, text='  SDR  ')
+tab_interface.add(eps_tab, text='  EPS  ')
+tab_interface.add(uhf_tab, text='  UHF  ')
+
+#Populates dropdown list (combobox option names) for the selected subsystem
+def get_dropdown_list(tab_index):
+    cmd_list = cmd_dictionary[tab_index]
+    dropdown_cmd_list = []
+    for key, value in cmd_list.items():
+        if(value.cmd_sendable == 1):
+            dropdown_cmd_list.append(value.cmd_description)
+    return(dropdown_cmd_list)
+
+#Populates command list for the selected subsystem (used for more technical tasks)
+def get_cmd_list(tab_index):
+    cmd_list = cmd_dictionary[tab_index]
+    return(cmd_list)
+
+
 ######################################################################################################################
 #Test Tab (currently in use for testing interface functions, will be removed later)
 #NOTE: All interface elements used in the test tab will be included in each subsystem tab
 
-tab_interface.add(test_tab, text='Test')
+#Test command list population
+test_dropdown = get_dropdown_list(0)
+test_cmd_list = get_cmd_list(0)
 
 #-------------------------------------------------------------------#
 #DROPDOWN COMMAND MENU
-#Populates command list for the selected subsystem (in this case, test (id = 1))
-cmd_list = cmd_dictionary[1]
-dropdown_cmd_list = []
-for key, value in cmd_list.items():
-    if(value.cmd_sendable == 1):
-        dropdown_cmd_list.append(value.cmd_description)
-
 #Creates a dropdown menu for command options
+#Uses command list for the selected subsystem
 ttk.Label(test_tab, text="Choose Command:", padding=3).grid(column=0,row=0,sticky=W)
 test_command = StringVar(test_tab)
-test_combobox = ttk.Combobox(test_tab,textvariable=test_command,values=dropdown_cmd_list,state=(["readonly"]))
+test_combobox = ttk.Combobox(test_tab,textvariable=test_command,values=test_dropdown,state=(["readonly"]),width=46)
 test_combobox.grid(column=2,row=0)
 
 #When the user selects a command in the combobox, looks for ID of selected command in the command manifest
 def get_dropdown_selection():
-    for key, value in cmd_list.items():
+    for key, value in test_cmd_list.items():
         if(value.cmd_description == test_command.get()):
             value.cmd_id = key
             #Returns command id for selected command
@@ -71,7 +88,7 @@ def get_dropdown_selection():
 #Checks if selected command (indicated by cmd_id) has a payload
 #Used to enable/disable input box
 def command_has_payload():
-    cmd = cmd_list[get_dropdown_selection()]
+    cmd = test_cmd_list[get_dropdown_selection()]
     if cmd.cmd_has_payload == 1:
         return TRUE
     else:
@@ -85,15 +102,16 @@ test_entry = ttk.Entry(test_tab, textvariable=packet_input)
 test_entry.grid(column=1,row=2,columnspan=3, sticky=EW)
 
 #Disables input box if a command has not been selected from the dropdown menu
-#OR disables input box if the command does not have a data payload (NOT YET IMPLEMENTED)
+#OR disables input box if the command does not have a data payload
 test_entry.state(['disabled'])
 def set_entry_state(var,index,mode):
     if test_command.get() == 0 or command_has_payload():
+        test_entry.delete(0,END)
         test_entry.state(['disabled'])
     else:
         test_entry.state(['!disabled'])
 test_command.trace_add("write", set_entry_state)
-      
+
 #Retrieves data from input box and selection from combobox
 def get_input():
     #Command id from dropdown selection will be used to call a specific command
@@ -125,6 +143,7 @@ packet_input.trace_add("write", set_button_state)
 
 ######################################################################################################################
 #OBC Tab (currently used for testing buttons)
+#ADD ANY PACKET TESTING CODE HERE
 
 #Commands for basic packet-sending testing
 def send_packet():
@@ -133,17 +152,32 @@ def send_packet():
 def req_packet():
     print("Packet Request Button Test")
 
-#Adds OBC tab to interface
-tab_interface.add(obc_tab, text='  OBC  ')
+#Adds buttons to interface
 send_packet_btn = ttk.Button(obc_tab, text="Send Packet", command=send_packet, padding=5).grid(column=0,row=0)
 req_packet_btn = ttk.Button(obc_tab, text="Request Packet", command=req_packet, padding=5).grid(column=1,row=0)
 
-######################################################################################################################
-#More tabs (commands will be added later)
 
-tab_interface.add(adcs_tab, text='  ADCS  ')
-tab_interface.add(sdr_tab, text='  SDR  ')
-tab_interface.add(eps_tab, text='  EPS  ')
-tab_interface.add(uhf_tab, text='  UHF  ')
+######################################################################################################################
+#ADCS Tab
+
+#ADCS command list population
+adcs_dropdown = get_dropdown_list(2)
+adcs_cmd_list = get_cmd_list(2)
+
+#-------------------------------------------------------------------#
+#DROPDOWN COMMAND MENU
+#Creates a dropdown menu for command options
+#Uses command list for the selected subsystem
+ttk.Label(adcs_tab, text="Choose Command:", padding=3).grid(column=0,row=0,sticky=W)
+adcs_command = StringVar(adcs_tab)
+adcs_combobox = ttk.Combobox(adcs_tab,textvariable=adcs_command,values=adcs_dropdown,state=(["readonly"]))
+adcs_combobox.grid(column=2,row=0)
+
+
+######################################################################################################################
+#More Tabs!
+#Will be added later
+
+#-------------------------------------------------------------------#
 
 root.mainloop()
