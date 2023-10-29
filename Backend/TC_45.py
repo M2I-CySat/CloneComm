207b69163d76037392c89fa16f3a41fa88838268
UltimateSteve99 <ultimatesteve99@gmail.com>
2023-10-29 14:15:02 -0500

    TC45 works

--- Backend/TC_45.py
+++ Backend/TC_45.py
@@ -25,11 +25,11 @@ def endian_swap_double(invar):
 
 def TC_45():
 
-    #response = requests.get("https://tle.ivanstanojevic.me/api/tle/25544")
-    text = "{\"@context\":\"https:\/\/www.w3.org\/ns\/hydra\/context.jsonld\",\"@id\":\"https:\/\/tle.ivanstanojevic.me\/api\/tle\/25544\",\"@type\":\"Tle\",\"satelliteId\":25544,\"name\":\"ISS (ZARYA)\",\"date\":\"2023-10-16T04:13:26+00:00\",\"line1\":\"1 25544U 98067A   23289.17600325  .00022066  00000+0  38962-3 0  9994\",\"line2\":\"2 25544  51.6421  89.2403 0004841 108.6565  62.5649 15.50277152420558\"}"
+    response = requests.get("https://tle.ivanstanojevic.me/api/tle/25544")
+    #text = "{\"@context\":\"https:\/\/www.w3.org\/ns\/hydra\/context.jsonld\",\"@id\":\"https:\/\/tle.ivanstanojevic.me\/api\/tle\/25544\",\"@type\":\"Tle\",\"satelliteId\":25544,\"name\":\"ISS (ZARYA)\",\"date\":\"2023-10-16T04:13:26+00:00\",\"line1\":\"1 25544U 98067A   23289.17600325  .00022066  00000+0  38962-3 0  9994\",\"line2\":\"2 25544  51.6421  89.2403 0004841 108.6565  62.5649 15.50277152420558\"}"
     #print("Status Code: "+str(response.status_code))
 
-    #text = response.text
+    text = response.text
 
     print("Response Contents:")
     print(text)
